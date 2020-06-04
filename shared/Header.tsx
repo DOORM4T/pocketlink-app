ef565c8128547de026d44ef51b562f2ccfbcc46f
Matt Seto <seanseto@gmail.com>
2020-06-04 08:40:08 -0700

    style header

--- shared/Header.tsx
+++ shared/Header.tsx
@@ -1,16 +1,38 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
-import { navOptionsProps } from "../routes/HomeStack";
 import { StackNavigationProp } from "react-navigation-stack/lib/typescript/src/vendor/types";
-import { NavigationRoute, NavigationParams } from "react-navigation";
+import {
+  NavigationRoute,
+  NavigationParams,
+  ThemeColors,
+} from "react-navigation";
+import { SimpleLineIcons } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function Header({ title, navigation }: HeaderProps) {
   return (
-    <View>
-      <Text>{title || "No Title"}</Text>
+    <View style={styles.header}>
+      {navigation.isFirstRouteInParent() && (
+        <MaterialCommunityIcons name="menu" size={30} color="#2699FB" />
+      )}
+      <Text style={styles.headerText}>{title || "No Title"}</Text>
+      <SimpleLineIcons name="options-vertical" size={24} color="#2699FB" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  headerText: {
+    color: "#2699FB",
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
 
 type HeaderProps = {
   title: string;
