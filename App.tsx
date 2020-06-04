ced4b5482ab345b8a5534b3bfe33d5b608406348
Matt Seto <seanseto@gmail.com>
2020-06-04 13:53:58 -0700

    add fonts

--- App.tsx
+++ App.tsx
@@ -1,6 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import HomeStack from "./routes/HomeStack";
+import * as Font from "expo-font";
+import { AppLoading } from "expo";
+
+const getFonts = () =>
+  Font.loadAsync({
+    "OpenSans-Regular": require("./assets/fonts/Open_Sans/OpenSans-Regular.ttf"),
+    "OpenSans-Bold": require("./assets/fonts/Open_Sans/OpenSans-Bold.ttf"),
+    "OpenSans-Light": require("./assets/fonts/Open_Sans/OpenSans-Light.ttf"),
+  });
 
 export default function App() {
-  return <HomeStack />;
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  if (fontsLoaded) return <HomeStack />;
+  else
+    return (
+      <AppLoading startAsync={getFonts} onFinish={() => setFontsLoaded(true)} />
+    );
 }
