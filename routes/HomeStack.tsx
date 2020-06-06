77f6e6936da06e58fdc5155edeb8bd929318df8e
Matt Seto <seanseto@gmail.com>
2020-06-06 12:22:48 -0700

    refactor navigation

--- routes/HomeStack.tsx
+++ routes/HomeStack.tsx
@@ -1,22 +1,10 @@
-import React from "react";
-import {
-  createAppContainer,
-  NavigationRouteConfigMap,
-  NavigationRoute,
-  NavigationParams,
-  CreateNavigatorConfig,
-  NavigationScreenConfigProps,
-} from "react-navigation";
+import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
-import {
-  StackNavigationOptions,
-  StackNavigationProp,
-} from "react-navigation-stack/lib/typescript/src/vendor/types";
-
-import Header from "../shared/Header";
 import Home from "../screens/Home";
 import Customize from "../screens/Customize";
 import Finished from "../screens/Finished";
+import { createHeader } from "./createHeader";
+import { stackNavScreens, navOptionsProps } from "./routes";
 
 // screens for stack navigation
 const screens: stackNavScreens = {
@@ -36,42 +24,14 @@ const screens: stackNavScreens = {
     screen: Finished,
     navigationOptions: ({ navigation }: navOptionsProps) => ({
       headerTitle: () => createHeader("FINISHED", navigation),
+      headerShown: false,
     }),
   },
 };
 
-// create & export navigator
-const HomeStack = createStackNavigator(screens);
+const HomeStack = createStackNavigator(screens, {
+  initialRouteName: "Home",
+  mode: "modal",
+});
+
 export default createAppContainer(HomeStack);
-
-//
-// HELPERS
-//
-
-/**
- * @description create a navigation Header with a custom title
- * @param title title of header
- * @param navigation prop for stack navigation
- */
-function createHeader(title: string, navigation: navigationProp) {
-  return <Header title={title} navigation={navigation} />;
-}
-
-//
-// TYPE ALIASES
-//
-export type stackNavScreens = NavigationRouteConfigMap<
-  StackNavigationOptions,
-  StackNavigationProp<NavigationRoute<NavigationParams>, NavigationParams>,
-  unknown
->;
-
-export type navOptionsProps = NavigationScreenConfigProps<
-  StackNavigationProp<NavigationRoute<NavigationParams>, NavigationParams>,
-  unknown
->;
-
-export type navigationProp = StackNavigationProp<
-  NavigationRoute<NavigationParams>,
-  NavigationParams
->;
